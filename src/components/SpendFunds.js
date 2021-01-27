921ddbfa60bb41c0a10e00d361789eebde39e349
rambhajansonti <rambhajansonti0597@gmail.com>
2021-01-27 11:59:08 +0530

    some css modification

--- src/components/SpendFunds.js
+++ src/components/SpendFunds.js
@@ -117,7 +117,7 @@ function SpendFunds() {
                style={{ marginTop: "5px" }}
                disabled={!isAmountValid || activeUser === undefined}
                onClick={spendAmount}
-               className="sbmt-btn"
+               // className="sbmt-btn"
             >
                Submit
             </Button>
